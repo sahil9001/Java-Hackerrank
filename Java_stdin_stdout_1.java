9e47b5fd64740e6ae392bdb0b2eb10e014da92ef
sahil9001 <sahilsilare@gmail.com>
2020-06-15 20:22:37 +0530

    new file:   Java_if_else.java 	new file:   Java_stdin_stdout_1.java

---
+++ Java_stdin_stdout_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Java_stdin_stdout_1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+    }   
+}
