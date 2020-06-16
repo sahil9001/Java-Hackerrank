9506ab51f0e11eb7dbea99a9b6403d358baf5ec7
sahil9001 <sahilsilare@gmail.com>
2020-06-16 10:48:48 +0530

    new file:   Java_eof.java

---
+++ Java_eof.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Java_eof {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int i=0;
+        while(sc.hasNextLine()){
+            i++;
+            System.out.println(i + " " + sc.nextLine());
+        }
+    }
+}
