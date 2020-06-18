6c2084388b3f8757b77e1e7870545d150bc3a21d
sahil9001 <sahilsilare@gmail.com>
2020-06-18 19:48:19 +0530

    new file:   Java_factory_pattern.java 	new file:   Java_prime_checker.java 	new file:   Java_static_init_block.java

---
+++ Java_prime_checker.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+import static java.lang.System.in;
+
+class Prime {
+    public void checkPrime(int ... nums){
+        for(int i:nums){
+            if(isPrime(i)){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+    boolean isPrime(int a){
+        int flag = 0;
+        if(a>=2){
+            for(int i=2;i<=a/2;i++){
+                if(a%i==0) flag++;
+                if(flag>0) return false;
+            }
+        }
+        else if(a<2){
+            return false;
+        }
+        return true;
+    }
+}
+public class Java_prime_checker {
+
+	public static void main(String[] args) {
+		try{
+		BufferedReader br=new BufferedReader(new InputStreamReader(in));
+		int n1=Integer.parseInt(br.readLine());
+		int n2=Integer.parseInt(br.readLine());
+		int n3=Integer.parseInt(br.readLine());
+		int n4=Integer.parseInt(br.readLine());
+		int n5=Integer.parseInt(br.readLine());
+		Prime ob=new Prime();
+		ob.checkPrime(n1);
+		ob.checkPrime(n1,n2);
+		ob.checkPrime(n1,n2,n3);
+		ob.checkPrime(n1,n2,n3,n4,n5);	
+		Method[] methods=Prime.class.getDeclaredMethods();
+		Set<String> set=new HashSet<>();
+		boolean overload=false;
+		for(int i=0;i<methods.length;i++)
+		{
+			if(set.contains(methods[i].getName()))
+			{
+				overload=true;
+				break;
+			}
+			set.add(methods[i].getName());
+			
+		}
+		if(overload)
+		{
+			throw new Exception("Overloading not allowed");
+		}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+}
+
